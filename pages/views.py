a9bc83691a362895c8775fc0c2459d67d6eb2321
= <=>
2020-09-10 19:32:44 -0300

    included templates/pages about and index

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>Hello world</h1>')
+    # return HttpResponse('<h1>Hello world</h1>')
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
