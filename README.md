61120460c7e8b3c86601a1048f69227572c86f6a
= <=>
2020-09-14 23:06:42 -0300

    migrations

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # django
 
 ````
-
+    pip install psycopg2-binary | psycopg2
+    * alter database [sqlite] for postgresql    
+        - python manage.py migrate
 ````
--- btre/settings.py
+++ btre/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR + '/db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': BASE_DIR + '/db.sqlite3', fixme - migrate.
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
+
     }
 }
 
