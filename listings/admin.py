9fc8770c17c0b49c451988f6034668525cba328e
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2020-09-20 20:48:35 -0300

    added functionallity for format admin area - listings, with filters for realtors

--- listings/admin.py
+++ listings/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Listing
-admin.site.register(Listing)
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+
+
+admin.site.register(Listing, ListingAdmin)
