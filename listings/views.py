65e1cfacb29d180a7c07e4e39943e32f89180f14
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2020-09-20 21:11:09 -0300

    WTF VIEW and data in templates listings.jinja2 beatifull

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+from .models import Listing
 
 # Create your views here.
 
+
 def index(request):
-    return render(request, 'listings/listings.jinja2')
+    return render(request, 'listings/listings.jinja2', {
+        'name': 'Machado'
+    })
 
 
 def listing(request):
