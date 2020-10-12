aa638c03ebb92075c3516fbfb96f4b6f74dc45a4
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2020-10-12 09:48:50 -0300

    improviments

--- listings/urls.py
+++ listings/urls.py
@@ -4,6 +4,6 @@ from . import views             # chamada para a rota da view.
 # /listings/23
 urlpatterns = [
     path('', views.index, name='listings'),
-    path('<int:listing_id>', views.listing, name='listing'),
+    path('<int:listing_id>', views.listing, name='listing'),        #  contact realtor [make an inquiry]
     path('search', views.search, name='search')
 ]
